1fecf9d791aead9a638815758a9ff373c0214cd2
Adrian Celczyński <adriancelczynski@icloud.com>
2020-03-21 14:28:31 +0100

    Initial Commit

---
+++ wmii-floyd-warshall/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wmii-floyd-warshall
+//
+//  Created by Adrian Celczyński on 21/03/2020.
+//  Copyright © 2020 Adrian Celczyński. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
